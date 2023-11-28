6314e30002f22ba42fea629aa3acef3f3dfecf5b
mariasolegallina <gallinamariasole@gmail.com>
2023-11-28 14:56:28 +0100

    add - class with properties

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// definizione della classe
+class Production{
+// proprietà della classe
+public $title;
+public $language;
+public $rating;
+}
+
+
+?>
+
+<!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
+ - è definita una classe Production
+   => all’interno della classe sono dichiarate delle variabili d’istanza (title, language e rating)
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo (get / set ad esempio)
+- vengono istanziati almeno due oggetti Production'e stampati a schermo i valori delle relative proprietà
+Bonus
+Creare un layout completo per stampare a schermo una lista di movies.
+Buon lavoro! -->
