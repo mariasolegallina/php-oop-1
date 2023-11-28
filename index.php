1e9ea8e1b4e3805a911c720ae02d2d39f8a7a117
mariasolegallina <gallinamariasole@gmail.com>
2023-11-28 15:16:11 +0100

    add - construct

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
 // definizione della classe
-class Production{
-// proprietà della classe
-public $title;
-public $language;
-public $rating;
+class Production {
+    // proprietà della classe
+    public $title;
+    public $language;
+    public $rating;
+
+    // costruttore 
+    function __construct($title, $language, $rating) {
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
+    }
+
+
 }
 
 
 ?>
-
-<!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
- - è definita una classe Production
-   => all’interno della classe sono dichiarate delle variabili d’istanza (title, language e rating)
-   => all’interno della classe è definito un costruttore
-   => all’interno della classe è definito almeno un metodo (get / set ad esempio)
-- vengono istanziati almeno due oggetti Production'e stampati a schermo i valori delle relative proprietà
-Bonus
-Creare un layout completo per stampare a schermo una lista di movies.
-Buon lavoro! -->
