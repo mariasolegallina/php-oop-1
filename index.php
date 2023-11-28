8d196b86e0893374128ff0e6ea1f65e9eb66afe7
mariasolegallina <gallinamariasole@gmail.com>
2023-11-28 16:10:09 +0100

    add - get_title

--- index.php
+++ index.php
@@ -14,8 +14,25 @@ class Production {
         $this->rating = $rating;
     }
 
+    // methods
+    function get_title() {
+        return $this->title;
+    }
 
 }
 
+$film1 = new Production('Zoolander', 'English', '3');
+$film2 = new Production('Boris', 'Italiano', '5');
 
+echo $film1->get_title();
 ?>
+
+<!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
+ - è definita una classe Production
+   => all’interno della classe sono dichiarate delle variabili d’istanza (title, language e rating)
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo (get / set ad esempio)
+- vengono istanziati almeno due oggetti Production'e stampati a schermo i valori delle relative proprietà
+Bonus
+Creare un layout completo per stampare a schermo una lista di movies.
+Buon lavoro! -->
