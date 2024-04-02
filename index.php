840e6e64f02eb3e177904e1578e1214e93a2301f
mariasolegallina <gallinamariasole@gmail.com>
2024-04-02 15:18:10 +0200

    add new Movie()

--- index.php
+++ index.php
@@ -2,12 +2,23 @@
 class Movie
 {
     public $title;
+    public $duration;
     public $year;
     public $cast;
     public $director;
     public $production;
 }
 
+$movie1 = new Movie();
+$movie1->title = "Poor things";
+$movie1->duration = 141;
+$movie1->year = 2023;
+$movie1->cast = "Emma Stone, Mark Ruffalo, Willem Dafoe";
+$movie1->director = "Yorgos Lanthimos";
+$movie1->production = "Element Pictures";
+
+
+var_dump($movie1);
 ?>
 
 <!DOCTYPE html>
