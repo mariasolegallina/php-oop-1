5b1afe11b3b0f796691c19966b48ae624b5f977c
mariasolegallina <gallinamariasole@gmail.com>
2024-04-02 15:32:46 +0200

    add construct in class Movie

--- index.php
+++ index.php
@@ -7,15 +7,25 @@ class Movie
     public $cast;
     public $director;
     public $production;
+
+    function __construct($_title, $_duration, $_year, $_cast, $_director, $_production)
+    {
+        $this->title = $_title;
+        $this->duration = $_duration;
+        $this->year = $_year;
+        $this->cast = $_cast;
+        $this->director = $_director;
+        $this->production = $_production;
+    }
 }
 
-$movie1 = new Movie();
-$movie1->title = "Poor things";
-$movie1->duration = 141;
-$movie1->year = 2023;
-$movie1->cast = "Emma Stone, Mark Ruffalo, Willem Dafoe";
-$movie1->director = "Yorgos Lanthimos";
-$movie1->production = "Element Pictures";
+$movie1 = new Movie("Poor things", 141, 2023, "Emma Stone, Mark Ruffalo, Willem Dafoe", "Yorgos Lanthimos", "Element Pictures");
+// $movie1->title = "Poor things";
+// $movie1->duration = 141;
+// $movie1->year = 2023;
+// $movie1->cast = "Emma Stone, Mark Ruffalo, Willem Dafoe";
+// $movie1->director = "Yorgos Lanthimos";
+// $movie1->production = "Element Pictures";
 
 
 var_dump($movie1);
