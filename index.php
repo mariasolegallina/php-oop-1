323a4570056ccc76609cbbc8beec6c27125b62be
mariasolegallina <gallinamariasole@gmail.com>
2024-04-02 14:45:04 +0200

    add var in class

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+class Movie
+{
+    public $title;
+    public $year;
+    public $cast;
+    public $director;
+    public $production;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
